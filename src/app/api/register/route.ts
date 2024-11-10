d30c0c440ff57d378a76df2ef2c7a77c47bbf5ee
Andrei <dinamo1999@icloud.com>
2024-11-11 00:32:37 +0100

    fix

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -20,6 +20,7 @@ function sendEmail(email: string) {
   // Set up transporter with Gmail SMTP
   const transporter = nodemailer.createTransport({
     service: 'gmail',
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
