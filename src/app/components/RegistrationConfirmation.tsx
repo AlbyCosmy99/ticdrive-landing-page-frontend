7b790f409e51d87b6e32866384ee35cf61672cfa
Andrei <dinamo1999@icloud.com>
2024-11-13 01:21:07 +0100

    feat: add registration confirmation and promotional banner

--- src/app/components/RegistrationConfirmation.tsx
+++ src/app/components/RegistrationConfirmation.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Progress } from "@/components/ui/progress";
 
-const RegistrationConfirmation: React.FC = () => {
-  return (
-    <div style={{ padding: '1em', textAlign: 'center', backgroundColor: '#f0f8ff', borderRadius: '8px' }}>
-      <p style={{ fontSize: '1.2em', color: '#333', fontWeight: 'bold' }}>
-        Iscrizione avvenuta con successo!
-      </p>
-      <p style={{ fontSize: '1em', color: '#555' }}>
-        Conferma la tua email per ricevere lo sconto del 15% e rimanere sempre aggiornato.
-      </p>
-    </div>
-  );
+const RegistrationConfirmation: React.FC<{ setProgressIsVisible: (loading: boolean) => void }> = ({ setProgressIsVisible }) => {
+    const [progress, setProgress] = useState(100);
+
+    useEffect(() => {
+        if (progress > 0) {
+            const timer = setInterval(() => {
+                setProgress((prevProgress) => Math.max(prevProgress - 0.6, 0));
+            }, 100);
+
+            return () => clearInterval(timer);
+        } else {
+            setProgressIsVisible(false);
+        }
+    }, [progress, setProgressIsVisible]);
+
+    return (
+        progress !== 0 && (
+            <div style={{ padding: '1em', textAlign: 'center', backgroundColor: '#f0f8ff', borderRadius: '8px' }}>
+                <p style={{ fontSize: '1.2em', color: '#00BF63', fontWeight: 'bold' }}>
+                    Iscrizione avvenuta con successo!
+                </p>
+                <p style={{ fontSize: '1em', color: '#737373' }}>
+                    Conferma la tua email per ricevere lo sconto del 15% e rimanere sempre aggiornato.
+                </p>
+                <div className='mt-2'>
+                    <Progress value={progress} />
+                </div>
+            </div>
+        )
+    );
 };
 
 export default RegistrationConfirmation;
--- src/app/components/SignUpButton.tsx
+++ src/app/components/SignUpButton.tsx
@@ -2,12 +2,14 @@ import ArrowUp from '../../../assets/arrowUp.svg';
 
 interface SignUpButtonProps {
   revertStyle?: boolean;
+  onClick?: () => void;
 }
 
-const SignUpButton: React.FC<SignUpButtonProps> = ({revertStyle = false}) => {
+const SignUpButton: React.FC<SignUpButtonProps> = ({revertStyle = false, onClick}) => {
   return (
     <button
       className={`flex gap-2 justify-center items-center h-fit p-3 px-5 rounded-full ${revertStyle ? 'bg-white' : 'bg-drive'}`}
+      onClick={onClick}
     >
       <h2 className={`${revertStyle ? 'text-drive' : 'text-white'} lg:text-xl`}>
         Iscriviti ora
