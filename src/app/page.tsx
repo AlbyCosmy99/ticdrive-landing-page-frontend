9a10e3237e959a68994c01c22f2bad6dddf864e2
Andrei <dinamo1999@icloud.com>
2024-11-13 22:02:58 +0100

    fix: promo banner red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,12 +50,11 @@ export default function Home() {
       <div
         ref={bannerRef}
         className="w-full bg-red-500 text-white font-bold text-sm text-center p-2 flex justify-evenly fixed top-0 z-50"
-        style={{ backgroundColor: '#00BF63' }}
       >
         <Link href="#registrati" passHref className='flex justify-evenly w-full'>
+          <p className='hidden lg:block'>Sconto del 15% sulla tua prima prenotazione!</p>
           <p>Sconto del 15% sulla tua prima prenotazione!</p>
-          <p>Sconto del 15% sulla tua prima prenotazione!</p>
-          <p>Sconto del 15% sulla tua prima prenotazione!</p>
+          <p className='hidden lg:block'>Sconto del 15% sulla tua prima prenotazione!</p>
         </Link>
       </div>
 
