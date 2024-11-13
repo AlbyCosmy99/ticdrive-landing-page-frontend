6d1bf75c1d99bf8187e6e6ca863065bd1a1e502f
Andrei <dinamo1999@icloud.com>
2024-11-13 22:19:00 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,17 @@ export default function Home() {
 
   return (
     <>
-
+      <hr className="bg-white w-full" />
+      <div
+        ref={bannerRef}
+        className=" w-full bg-red-500 text-white font-bold text-sm text-center p-2 flex justify-evenly items-center fixed top-0 z-50"
+      >
+        <Link href="#registrati" passHref className='flex justify-evenly w-full h-5'>
+          <p className='hidden lg:block'>Sconto del 15% sulla tua prima prenotazione!</p>
+          <p>Sconto del 15% sulla tua prima prenotazione!</p>
+          <p className='hidden lg:block'>Sconto del 15% sulla tua prima prenotazione!</p>
+        </Link>
+      </div>
 
       <div style={{ paddingTop: bannerHeight }}>
         <NavBar />
