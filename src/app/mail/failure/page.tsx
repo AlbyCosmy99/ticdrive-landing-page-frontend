0b8372971d1c2e6ae3f507b3cb1a9592aa1ea6f9
Andrei <dinamo1999@icloud.com>
2024-11-14 23:49:05 +0100

    feat: implement failure page

--- src/app/mail/failure/page.tsx
+++ src/app/mail/failure/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link"
+
 const Failure = () => {
     return (
-        <p>Failure</p>
+        <div className="flex flex-col justify-center items-center min-h-screen p-2">
+            <h1>Errore durante la verifica la mail. Riprova.</h1>
+            <h2 className="underline"><Link href='https://www.ticdrive.it'>Vai alla pagina di <span className="text-tic">Tic</span><span className="text-drive">Drive</span></Link></h2>
+        </div>
     )
 }
 
