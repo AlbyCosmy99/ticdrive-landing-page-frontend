dac9fafe798f7b9f0a4be3ba3d8a08e39cb0b991
Andrei <dinamo1999@icloud.com>
2024-11-14 12:01:53 +0100

    fix

--- src/app/mail/confirmation/[token]/page.tsx
+++ src/app/mail/confirmation/[token]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Spinner from "@/app/components/ui/Spinner"
 import { useParams, useRouter } from "next/navigation"
 import { useEffect } from "react"
 
@@ -9,25 +10,32 @@ const MailConfirmation = () => {
 
     useEffect(() => {
         if(!token) {
-            router.replace('/success')
+            router.replace('/mail/confirmation')
         }
 
         const checkToken = async () => {
-            const response = await fetch(`${process.env.BACKEND}/api/check-token/token=${token}`)
-            const tokenIsValid = await response.json()
-            if(tokenIsValid) {
-                console.log(tokenIsValid)
-                router.replace('/mail/success/' + token)
-            } else {
-
+            try {
+                const response = await fetch(`https://landing-page-users-ticdrive-backend.onrender.com/api/check-token/token=${token}`)
+                const tokenIsValid = await response.json()
+                if(tokenIsValid) {
+                    console.log(tokenIsValid)
+                    router.replace('/mail/success/' + token)
+                } else {
+                    router.replace('/mail/failure')
+                }
+            } catch(error) {
+                console.log(error)
+                router.replace('/mail/failure')
             }
         }
-
         checkToken()
     }, [])
  
     return (
-        <p>pagina di controllo. Token: {token}</p>
+        <div className="flex flex-col justify-center items-center min-h-screen">
+            <p className="mb-2">Verificando la tua mail. Attendere...</p>
+            <Spinner />
+        </div>
     )
 }
 
