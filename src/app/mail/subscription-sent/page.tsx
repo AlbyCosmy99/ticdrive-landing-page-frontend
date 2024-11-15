afa039d2c1021ae55dfdc5ee7f5cd419f1bde987
Andrei <dinamo1999@icloud.com>
2024-11-15 01:15:03 +0100

    fix

--- src/app/mail/subscription-sent/page.tsx
+++ src/app/mail/subscription-sent/page.tsx
@@ -6,7 +6,7 @@ const SubscriptionSent = () => {
         <div className="flex flex-col justify-center items-center min-h-screen p-4">
             <Image width={125} height={125} src="https://albycosmy99.github.io/landing-page-users-ticDrive-backend/assets/TicDriveLogo.jpeg" alt="TicDrive Logo" />
             <div className="text-center mt-5">
-                <h1 className="font-bold text-2xl mb-3">Benvenuto nel mondo <span className="text-tic">Tic</span><span className="text-drive">Drive</span>!</h1>
+                <h1 className="font-bold text-2xl mb-3">Conferma la tua mail!</h1>
                 <p>Ciao, la tua richiesta è arrivata.</p>
                 <p>Ti abbiamo inviato una mail di conferma.</p>
                 <p>Basta confermare e ti mandiamo il codice sconto, facile facile!</p>
