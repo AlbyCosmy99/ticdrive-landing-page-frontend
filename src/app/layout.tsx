dd232433ae3a4e284a0fbb7757a24bbf4cee1c37
Andrei <dinamo1999@icloud.com>
2024-11-16 12:41:28 +0100

    feat: add facebook api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,27 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             gtag('config', 'G-CEHL3QKF3T');
           `}
         </Script>
+  
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1290204491971799');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1290204491971799&ev=PageView&noscript=1"
+          />
+        </noscript>
         <SpeedInsights />
         <Analytics />
         {children}
