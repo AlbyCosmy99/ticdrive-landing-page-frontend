4bb2d6eb5619477c332b641d78d9ea94c1a3d7be
Andrei <dinamo1999@icloud.com>
2024-11-16 12:05:38 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,8 +103,8 @@ export default function Home() {
     </div>
     {/* Secondo banner */}
     <div
-      style={{ height: '2.5rem', top: '2.5rem' }}
-      className="w-full bg-yellow-500 text-white font-bold text-sm text-center p-2 flex justify-evenly  fixed z-50"
+      style={{ height: '2.5rem', top: '2.5rem' }} 
+      className="w-full bg-yellow-500 text-white font-bold text-sm text-center p-2 flex justify-evenly lg:items-center fixed z-50"
       role="banner"
       aria-label="Free waiting list banner"
       onClick={handleBannerClick}
